5901635d3935b32abf9db3792fcc885f196c7a72
Gracie2-png <malukigracie@gmail.com>
2022-10-31 10:50:42 -0700

    [ADD] add palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,35 @@
+// program to check if a string or number is a palindrome
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    int length;
+    bool isPalindrome = true;
+
+    cout << "Enter word or number: ";
+
+    cin>>str;
+
+    length = str.length();
+
+    for (int i = 0; i < (length / 2); i++)
+    {
+        if (str[i] != str[(length - 1) - i])
+            isPalindrome = false;
+    }
+
+    if (isPalindrome == true)
+        cout << str << " is a palindrome" << endl;
+
+    else
+        cout << str << " is not a palindrome" << endl;
+
+//new text begins here
+    return 0;
+//new text ends here
+}
+
